45b6515420b0d931af14152404d72d6f75c5376b
YelyzavetaMyshenyova <46732280+YelyzavetaMyshenyova@users.noreply.github.com>
2019-09-29 14:25:49 -0500

    create objects

--- javascript.js
+++ javascript.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
+  
 $("form").submit(function(){
   alert("Submitted");
+  
 });
+  
 });
+
+function Person(fname,sname){
+        this.firstname=firstname;
+        this.lastname=lastname;   
+        this.number=number;
+}
+
+function submit(){
+var person = new Person(document.getElementById('firstname').value, document.getElementById('lastname').value, document.getElementById('number').value);
+}
